4a369430c9abdd80e33e6e7219c2c6eed4c7d62c
katisalminen <50317491+katisalminen@users.noreply.github.com>
2026-01-09 18:14:14 +0200

    refactor joint renamer v3 core function

--- rigging/joint-renamer/joint_renamer.py
+++ rigging/joint-renamer/joint_renamer.py
@@ -3,15 +3,17 @@ JOINT RENAMING TOOL
 
 V3:
 
-    - Dropdowns for joint type, side, region
-    - Checkboxes for indexing, skipping already named joints
-    - <prefix>_<side>_<region>_<basename><index>
-    - Still name the entire chain in one go
-    - Auto end bone prefixes be_, hbe_, ebe_
     - Refactor rename(), split into three functions:
         - Joint chain validation
         - Deciding new names
         - Renaming
+    
+    - Skip & index toggle, no UI yet
+    - Joint type, side, region naming, no UI yet
+    - <prefix_><side_><region_><basename><index>
+    - Still name the entire chain in one go
+    - Auto end bone prefixes be_, hbe_, ebe_
+    - UI controls: index, skip, prefix dropdown
 
 
 V4:
@@ -31,8 +33,6 @@ Future improvements:
 
 import maya.cmds as cmds
 
-# CORE
-
 prefixes = {
     "Default": "bn_",
     "Hair": "hbn_",
@@ -41,44 +41,28 @@ prefixes = {
     }
 
 prefix_values = tuple(prefixes.values())
-index_padding = 2
 
+def short_name(dag_path: str) -> str:
+    if not dag_path:
+        return "" # this shouldn't happen
+    return dag_path.split("|")[-1]
 
-def is_named(name: str):
-    if name.startswith(prefix_values):
-        return True
-    else:
-        return False
+def is_named(name: str) -> bool:
+    return short_name(name).startswith(prefix_values)
     
-def check_children(joint: str):
-    children = cmds.listRelatives(joint, type="joint")
-    if not children:
-        children = []
-    return children
+def check_children(joint: str) -> list[str]:
+    children = cmds.listRelatives(joint, type="joint", fullPath=True) or []
+    return children # current joint's child(ren)
 
-def rename(user_name: str, skip: bool, use_index: bool, index: int):
 
-    sel = cmds.ls(selection=True)
+
+# FUNCTION 1: Validate user's selection
+
+def validate() -> list[str]:
+
+    sel = cmds.ls(selection=True, long=True) # list things in the current scene (selected ones)
     joint_chain = []
-    joint_chain_info = []
 
-
-    # VALIDATE NAME
-
-    clean_name = user_name.strip()
-
-    if not clean_name:
-        raise RuntimeError("Joint name cannot be empty.")
-    elif clean_name[0].isdigit():
-        raise RuntimeError("Name cannot start with a digit.")
-
-    for c in clean_name:
-        if not c.isalnum() and c != "_":
-            raise RuntimeError("Name should only have letters A-Z, digits or underscores.")
-
-
-
-    ## VALIDATE SELECTION
     if not sel:
         raise RuntimeError("Please select the start of the joint chain.")
     elif len(sel) == 1:
@@ -88,8 +72,7 @@ def rename(user_name: str, skip: bool, use_index: bool, index: int):
             current_joint = sel[0]
     else:
         raise RuntimeError("Please select only one joint.")
-
-    ## CHECK FOR BRANCHING
+    
     while True:
         joint_children = check_children(current_joint)
         if len(joint_children) > 1:
@@ -100,54 +83,92 @@ def rename(user_name: str, skip: bool, use_index: bool, index: int):
                 break
             else:
                 current_joint = joint_children[0]
+    
+    return joint_chain
 
-    ## CHECK WHICH JOINTS ARE ALREADY NAMED
-    for joint in joint_chain:
+
+
+# FUNCTION 2: Decide the new names
+
+def define_name(chain: list[str], basename: str, should_skip: bool, use_index: bool) -> list:
+
+    clean_name = basename.strip()
+    index_padding = 2
+    index = 1
+    name_plan = [] 
+        # a list of dictionaries: og_name, new_name, index, will_rename, skip, end joint
+
+# validate
+
+    if not clean_name:
+        raise RuntimeError("Joint name cannot be empty.")
+    elif clean_name[0].isdigit():
+        raise RuntimeError("Name cannot start with a digit.")
+
+    for c in clean_name:
+        if not c.isalnum() and c != "_":
+            raise RuntimeError("Name should only have letters A-Z, digits or underscores.")
+        
+    if not chain:
+        raise RuntimeError("Internal error: joint chain is empty.")
+
+# is it named already?
+
+    for joint in chain:
         joint_dict = {
             "og_name": joint, 
             "already_named": is_named(joint)
             }
-        joint_chain_info.append(joint_dict)
+        name_plan.append(joint_dict)
 
-    ## RENAMING YES/NO
-    for joint in joint_chain_info:
-        if joint.get("already_named"):
-            if not skip:
-                joint["will_rename"] = True
-            else:
-                joint["will_rename"] = False
+# should the joint be renamed?
+
+    for joint in name_plan:
+        joint["will_rename"] = not (joint["already_named"] and should_skip)
+
+    for joint in name_plan: # obsolete but potentially needed later, otherwise remove
+        if joint["will_rename"]:
+            joint["basename"] = clean_name 
         else:
-            joint["will_rename"] = True
+            joint["basename"] = None
 
+# indexing
 
-    ## BASENAME
-    for joint in joint_chain_info:
+    for joint in name_plan:
 
-        if joint.get("will_rename"):
-            joint["basename"] = clean_name
-
-
-    ## INDEXING
-    for joint in joint_chain_info:
-
-        if use_index and joint.get("will_rename"):
+        if use_index and joint["will_rename"]:
             joint["index"] = index
             index += 1
+        else:
+            joint["index"] = None
 
+# finish building name_plan[]
 
-    ## RENAME JOINTS
-    for joint in joint_chain_info:
+    for joint in name_plan:
 
-        if joint.get("will_rename"):
+        if joint["will_rename"]:
             
-            if joint.get("index"):
-                index_str = str(joint.get("index")).zfill(index_padding)
+            if joint["index"] is not None:
+                index_str = str(joint["index"]).zfill(index_padding)
             else:
                 index_str = ""
 
-            new_name = f"bn_{joint.get('basename')}{index_str}"
-            
-            cmds.rename(joint["og_name"], new_name)
+            joint["new_name"] = f"bn_{joint['basename']}{index_str}"
+        
+        else:
+            joint["new_name"] = None
+
+
+    return name_plan
+
+# FUNCTION 3: Rename!!
+
+def apply_rename(plan: list[dict]):
+
+    for joint in reversed(plan):
+        if joint["new_name"]:
+            cmds.rename(joint["og_name"], joint["new_name"])
+
 
 
 
@@ -173,7 +194,7 @@ def show_ui():
 
         )
 
-        rename(user_input,True,True,1)
+        apply_rename(user_input,True,True,1)
 
 
     if cmds.window(window_id, exists=True):
