f265e718f2b3cb2d9e8389347a06203c40d15666
katisalminen <50317491+katisalminen@users.noreply.github.com>
2026-01-12 20:15:30 +0200

    continue joint renamer v3 needs fix

--- rigging/joint-renamer/joint_renamer.py
+++ rigging/joint-renamer/joint_renamer.py
@@ -3,9 +3,6 @@ JOINT RENAMING TOOL
 
 V3:
     
-    - Rethink system for storing and accessing prefixes
-    - Skip & index toggle, no UI yet
-    - Joint type, side, region naming, no UI yet
     - <prefix_><side_><region_><basename><index>
     - UI controls: index, skip, prefix dropdown
 
@@ -28,14 +25,32 @@ Future improvements:
 import maya.cmds as cmds
 import re
 
-prefixes = {
+pf = {
     "Default": "bn_",
     "Hair": "hbn_",
     "Eyelid": "ebn_",
     "End bone": "be_"
     }
 
-prefix_values = tuple(prefixes.values())
+all_pf = tuple(pf.values())
+default_pf = pf["Default"]
+hair_pf = pf["Hair"]
+eyelid_pf = pf["Eyelid"]
+end_pf = pf["End bone"]
+
+side = {
+    "None": "",
+    "Left": "l_",
+    "Right": "r_"
+    }
+
+region = {
+    "None": "",
+    "Upper": "upper_",
+    "Middle": "middle_",
+    "Lower:": "lower_"
+    }
+
 
 def short_name(dag_path: str) -> str:
     if not dag_path:
@@ -43,7 +58,7 @@ def short_name(dag_path: str) -> str:
     return dag_path.split("|")[-1]
 
 def is_named(name: str) -> bool:
-    return short_name(name).startswith(prefix_values)
+    return short_name(name).startswith(all_pf)
     
 def check_children(joint: str) -> list[str]:
     children = cmds.listRelatives(joint, type="joint", fullPath=True) or []
@@ -85,17 +100,24 @@ def validate() -> list[str]:
 
 # FUNCTION 2: Decide the new names
 
-def define_name(chain: list[str], basename: str, should_skip: bool, use_index: bool) -> list:
+def define_name(
+        chain: list[str],
+        prefix: str,
+        side: str,
+        region: str,
+        basename: str,
+        should_skip: bool,
+        use_index: bool
+        ) -> list:
 
+    if not chain:
+        raise RuntimeError("Internal error: joint chain is empty.")
+    
     clean_name = basename.strip()
     index_padding = 2
     index = 1
     name_plan = [] 
-
-    def is_end(j): return j == chain[-1]
-
-    if not chain:
-        raise RuntimeError("Internal error: joint chain is empty.")
+    end_joint = chain[-1]
 
     if not re.fullmatch(r"[A-Za-z_][A-Za-z0-9_]*", clean_name):
         raise RuntimeError("Name must start with a letter or underscore and contain only A-Z, digits, or underscores.")
@@ -103,17 +125,19 @@ def define_name(chain: list[str], basename: str, should_skip: bool, use_index: b
     for joint in chain:
         already_named = is_named(joint)
         will_rename = not (already_named and should_skip)
-        end = is_end(joint)
-        prefix = prefix_values[0] if not end else prefix_values[3]
+        is_end_joint = (joint == end_joint)
+        prefix = prefix if not is_end_joint else end_pf
 
         joint_dict = {
             "og_name": joint, 
             "already_named": already_named,
-            "is_end": end,
+            "is_end": is_end_joint,
             "will_rename": will_rename,
+            "side": side,
+            "region": region,
             "prefix": prefix if will_rename else None,
             "basename": clean_name if will_rename else None,
-            "index": None,
+            "index": None
             }
         
         if use_index and will_rename:
@@ -131,7 +155,7 @@ def define_name(chain: list[str], basename: str, should_skip: bool, use_index: b
             else:
                 index_str = ""
             
-            joint["new_name"] = f"{joint['prefix']}{joint['basename']}{index_str}"
+            joint["new_name"] = f"{joint['prefix']}{joint['side']}{joint['region']}{joint['basename']}{index_str}"
         
         else:
             joint["new_name"] = None
@@ -153,40 +177,69 @@ def apply_rename(plan: list[dict]):
 
 def show_ui():
 
-    window_id = "joint_renamer_v2"
-    window_title = "Joint Renamer V2"
+    window_id = "joint_renamer_v3"
+    window_title = "Joint Renamer V3"
     basename_label = "Basename:"
 
     window_width = 350
     half = window_width//2
+    third = window_width//3
     label_w = window_width//5
+
+    mode_all = "Rename All"
+    mode_one = "Rename One By One"
+
+    can_use_index = True
+
+    def update_index_status():
+        if cmds.optionMenu(mode_menu, query=True, value=True) == mode_all:
+            can_use_index = False
 
     def onClose(*args):
         cmds.deleteUI(window_id)
 
+    def onSkip(*args):
+        pass
+
     def onRename(*args):
-        user_input = cmds.textFieldGrp(
-            basename_field,
-            query = True,
-            text = True
+        mode_type = cmds.optionMenu(mode_menu, query=True, value=True)
+        if mode_type == mode_all:
+            user_input = cmds.textFieldGrp(
+                basename_field,
+                query = True,
+                text = True)
+            use_index = True
+            should_skip = cmds.checkBox(skip_cb, query=True, value=True)
+            prefix_label = pf[cmds.optionMenu(joint_type_menu, query=True, value=True)]
+            side_label = side[cmds.optionMenu(side_menu, query=True, value=True)]
+            region_label = region[cmds.optionMenu(region_menu, query=True, value=True)]
+            rename_chain = validate()
+            rename_chain_dict = define_name(
+                rename_chain,
+                prefix_label,
+                side_label,
+                region_label,
+                user_input,
+                should_skip,
+                use_index)
+            apply_rename(rename_chain_dict)
 
-        )
-
-        apply_rename(user_input,True,True,1)
-
+        elif mode_type == mode_one:
+            use_index = cmds.checkBox(index_cb, query=True, value=True)
 
     if cmds.window(window_id, exists=True):
         cmds.deleteUI(window_id)
 
+#######
+
     cmds.window(
         window_id, 
         title=window_title, 
-        widthHeight=(window_width, 125),
+        widthHeight=(window_width, 185),
         sizeable=False
         )
     
     cmds.columnLayout(adjustableColumn=True)
-
     cmds.separator(height=20, style="none")
 
     basename_field = cmds.textFieldGrp(
@@ -196,22 +249,72 @@ def show_ui():
         ann="Type joint's base name here."
         )
     
-    cmds.separator(
-        height=50, 
-        style="none"
-        )
+    cmds.separator(height=10, style="none")
 
     cmds.rowLayout(numberOfColumns=2)
+    cmds.columnLayout(adjustableColumn=True)
+
+    joint_type_menu = cmds.optionMenu(label="Joint Type")
+    cmds.menuItem(label="Default")
+    cmds.menuItem(label="Hair")
+    cmds.menuItem(label="Eyelid")
+
+    side_menu = cmds.optionMenu(label="Side")
+    cmds.menuItem(label="None")
+    cmds.menuItem(label="Left")
+    cmds.menuItem(label="Right")
+
+    region_menu = cmds.optionMenu(label="Region")
+    cmds.menuItem(label="None")
+    cmds.menuItem(label="Upper")
+    cmds.menuItem(label="Middle")
+    cmds.menuItem(label="Lower")
+
+    cmds.setParent("..")
+   
+    cmds.columnLayout(adjustableColumn=True)
+   
+    index_cb = cmds.checkBox(
+        label="Numerical Indexing",
+        width=half,
+        value=True,
+        enable=can_use_index
+        )
+    
+    skip_cb = cmds.checkBox(
+        label="Skip Named Joints",
+        width=half,
+        value=True
+        )
+    
+    cmds.setParent("..")
+    cmds.setParent("..")
+    cmds.separator(height=10, style="none")
+    
+    mode_menu = cmds.optionMenu(label="Mode",width=half)
+    cmds.menuItem(label=mode_one,changeCommand=update_index_status)
+    cmds.menuItem(label=mode_all,changeCommand=update_index_status)
+
+
+    cmds.separator(height=10, style="none")
+
+    cmds.rowLayout(numberOfColumns=3)
     
     cmds.button(
         label="Rename",
-        width=half,
+        width=third,
         command=onRename
         )
     
     cmds.button(
+        label="Skip",
+        width=third,
+        command=onSkip
+        )
+    
+    cmds.button(
         label="Close",
-        width=half,
+        width=third,
         command=onClose
         )
     
@@ -224,8 +327,7 @@ show_ui()
 
 
 
-
-# UI
+# UI PLAN
 ## show current name
 ## joint type: regular, hair, eyelid
 ## joint side: none, left, right
