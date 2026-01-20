67e20f15e3bec936b1ec966bb579581e014de4e8
katisalminen <katisalminen@icloud.com>
2026-01-20 20:21:34 +0200

    add LRA tool UI, update gitignore for Mac

--- rigging/joint-LRA/joint-LRA.py
+++ rigging/joint-LRA/joint-LRA.py
@@ -1,14 +1,6 @@
 
 import maya.cmds as cmds
 
-
-mode = "hierarchy"
-# "single", "hierarchy", or "all"
-
-vis = 1
-# 0 or 1
-
-
 def toggle(mode: str, vis: int):
 
     if mode not in ("single", "hierarchy", "all"):
@@ -53,4 +45,72 @@ def toggle(mode: str, vis: int):
 
     print(f"{len(joint_list)} joint LRA set to {vis}.")
 
-toggle(mode, vis)
+
+def show_ui():
+
+    def read_state(*args):
+        value = cmds.intSlider(on_or_off, query=True, value=True)
+        return value
+
+    def onSelB(*args):
+        toggle("single", read_state())
+
+    def onHieB(*args):
+        toggle("hierarchy", read_state())
+    
+    def onAllB(*args):
+        toggle("all", read_state())
+
+    def onClose(*args):
+        cmds.deleteUI(window_id)
+
+
+    window_id = "lra_toggle"
+    window_title = "LRA Toggle"
+
+    window_width = 180
+    window_height = 200
+    window_padding = 20
+    window_content_width = window_width-window_padding*2
+    big_separator = 20
+    mini_separator = 5
+    third = window_content_width//3
+
+    if cmds.window(window_id, exists=True):
+        cmds.deleteUI(window_id)
+
+    cmds.window(
+        window_id, 
+        title=window_title, 
+        widthHeight=(window_width, window_height),
+        sizeable=False
+        )
+        
+    cmds.columnLayout(columnOffset=["both", window_padding], adj=True)
+    cmds.separator(height=big_separator, style="none")
+    cmds.rowLayout(numberOfColumns=3)
+
+    cmds.text(l="OFF", width=third)
+
+    on_or_off = cmds.intSlider(minValue=0,maxValue=1, width=third)
+
+    cmds.text(l="ON", width=third)
+
+    cmds.setParent("..")
+    cmds.separator(height=big_separator, style="none")
+
+    selection_button = cmds.button(l="Toggle Selection", command=onSelB)
+    cmds.separator(height=mini_separator, style="none")
+
+    hierarchy_button = cmds.button(l="Toggle Hierarchy", command=onHieB)
+    cmds.separator(height=mini_separator, style="none")
+
+    all_button = cmds.button(l="Toggle All", command=onAllB)
+
+    cmds.separator(height=big_separator, style="none")
+
+    close_button = cmds.button(l="Close", command=onClose)
+
+    cmds.showWindow(window_id)
+
+show_ui()
